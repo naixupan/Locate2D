27a22bf747c708714dbed13f643d1e2c4cfeffc7
何广鹏 <86814805+naixupan@users.noreply.github.com>
2025-05-22 09:36:06 +0800

    目标位置计算修改

--- dev_code/Locate0521.py
+++ dev_code/Locate0521.py
@@ -18,7 +18,7 @@ import cv2
 import numpy as np
 from utils_dev import *
 
-def compute_aruco_pose(image_path,K,distCoeffs,marker_size, save_image=True):
+def compute_aruco_pose(image_path,K,distCoeffs,marker_size, save_image=False):
 
     image, corners, ids = detect_aruco_marks(image_path)
     if ids is not None:
@@ -28,8 +28,8 @@ def compute_aruco_pose(image_path,K,distCoeffs,marker_size, save_image=True):
         print(f"相机位置：{camera_in_mark}")
         if save_image:
             visualize_pose(image, rvec, tvec, corners, ids, mark_in_camera, camera_in_mark, K, distCoeffs)
-            R_arr = np.array(R, dtype=np.float64)
-    t_arr = np.array(tvec, dtype=np.float64).flatten()
+        R_arr = np.array(R, dtype=np.float64)
+        t_arr = np.array(tvec, dtype=np.float64).flatten()
     # t_arr_ = t_arr * 1000
 
     matrix_ = np.eye(4, dtype=np.float32)
@@ -43,37 +43,46 @@ def compute_aruco_pose(image_path,K,distCoeffs,marker_size, save_image=True):
 
 
 if __name__ == '__main__':
-    camera_parameter_path = './Parameters/camera_params0513_chess.npz'
-    calibrate_matrix_path = './Parameters/cam2gripper_matrix0513_chess.npy'
-    target2mark_matrix_path = './Parameters/target2mark_matrix0521.npy'
+    camera_parameter_path = '../Parameters/camera_params0513_chess.npz'
+    calibrate_matrix_path = '../Parameters/cam2gripper_matrix0513_chess.npy'
+    target2mark_matrix_path = '../Parameters/target2mark_matrix0521.npy'
 
-    date = '0513'
-    count = '36'
+    date = '0521'
+    count = '1'
 
-    aruco_image_path = [f'./Images/test/ArUcoTest{date}-{count}-1.bmp',f'./Images/test/ArUcoTest{date}-{count}-2.bmp', f'./Images/test/ArUcoTest{date}-{count}-3.bmp']
+    aruco_image_path = [f'../Images/ArUcoImages/ArUcoTest{date}-{count}-1.bmp',f'../Images/ArUcoImages/ArUcoTest{date}-{count}-2.bmp', f'../Images/ArUcoImages/ArUcoTest{date}-{count}-3.bmp']
 
     K , distCoeffs = load_camera_parameters(camera_parameter_path)
     cam2gripper = load_calibrate_matrix(calibrate_matrix_path)
     T_target2mark = load_calibrate_matrix(target2mark_matrix_path)
 
-    robot_pose = [47.4705, -720.6140, -111.1109, 179.6020, -1.4580, -89.3855]
+    robot_pose = [81.4871, -806.8013, -110.4444, -179.5617, 0.9846, -90.2828]
     gripper2base = Pose2HomogeneousMatrix(robot_pose)
 
     toltal_pose = [0,0,0,0,0,0]
+    target_pose_total = [0, 0, 0, 0, 0, 0]
     for i in range(3):
-        mark2camera = compute_aruco_pose(aruco_image_path[i],K,distCoeffs,0.03,True)
+        mark2camera = compute_aruco_pose(aruco_image_path[i],K,distCoeffs,0.03)
         mark2base = gripper2base @ cam2gripper @ mark2camera
+        
+        T_target2base = mark2base @ T_target2mark
+        target_pose = HomogeneousMatrix2Pose(T_target2base)
         mark_pose = HomogeneousMatrix2Pose(mark2base)
-        print(f"目标位姿[{i}]：{mark_pose}")
+        print(f"Mark点位姿[{i}]：{mark_pose}")
+        print(f'目标点位姿：{target_pose}')
         for j in range(6):
             toltal_pose[j] += mark_pose[j]
+            target_pose_total[j] += target_pose[j]
 
     for i in range(6):
         toltal_pose[i] = toltal_pose[i]/3
-    print(f"平均目标位姿：{toltal_pose}")
+        target_pose_total[i] = target_pose_total[i]/3
+    print(f"平均Mark点目标位姿：{toltal_pose}")
+    print(f'平均目标点位姿：{target_pose_total}')
     
     T_mark2base = Pose2HomogeneousMatrix(toltal_pose)
-    T_target2base = T_mark2base @ T_target2mark
-    target_pose = HomogeneousMatrix2Pose(T_target2base)
-    print(f'偏置位姿：{target_pose}')
+    T_target2base_ = T_mark2base @ T_target2mark
+    target_pose_ = HomogeneousMatrix2Pose(T_target2base_)
+    print(f'(使用平均Mark点位姿计算)目标点位姿：{target_pose_}')
+    
 
