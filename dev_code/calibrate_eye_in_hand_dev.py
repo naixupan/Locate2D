75731539330873117d3e218eda4b126f745c3c74
何广鹏 <86814805+naixupan@users.noreply.github.com>
2025-06-15 11:49:10 +0800

    手眼标定代码更新

--- dev_code/calibrate_eye_in_hand_dev.py
+++ dev_code/calibrate_eye_in_hand_dev.py
@@ -199,7 +199,7 @@ def calibrate_(user_input):
 
     try:
         config = json.loads(user_input)
-        # config = eval(config)
+        config = eval(config)
     except Exception as e:
         logging.error(e)
         error_code = 401
@@ -212,10 +212,7 @@ def calibrate_(user_input):
         image_number = config.get("image_number")   # 图像数量
         pose_file_name = config.get("pose_file_name")   # 机械臂位姿文件名称
         parameter_save_path = config.get("parameter_save_path")     # 参数保存路径
-        calibrate_patameter = config.get("calibrate_patameter")     # 手眼标定参数名称
         image_save_path = config.get("image_save_path")     #手眼标定结果图片保存路径
-        calibrate_camera = config.get("calibrate_camera")   # 是否进行相机内参标定  1，是；0，否
-        camera_parameter = config.get("camera_parameter")   # 相机内参名称
         pattern_size = config.get("pattern_size")  # 标定板规格，类型为字符串如"9 16"，使用空格隔开
         square_size = config.get("square_size")     # 标定板单位尺寸，类型为字符串
         serial_number = config.get("serial_number") # 序列号
@@ -227,28 +224,37 @@ def calibrate_(user_input):
         return result_data
 
     pose_file_path = os.path.join(image_path, pose_file_name)
-    calibrate_patameter_save_path = os.path.join(parameter_save_path, calibrate_patameter)
-    is_calibrate_camera = int(calibrate_camera)
+    calibrate_patameter_save_path = f"{parameter_save_path}/camera2gripper_{serial_number}_chess.npy"
+    camera_parameter_save_path = f"{parameter_save_path}/camera_parameter_{serial_number}_chess.npz"
+    image_number = int(image_number)
+
 
     pattern_size_list = pattern_size.split(' ')
     pattern_size_turple = tuple(int(data) for data in pattern_size_list)
+    
+    print(pattern_size_turple)
     square_size = float(square_size)
 
     poses_list = read_robot_poses(pose_file_path)
+    
+    K, distCoeffs = calibrate_camera(image_path, image_number, serial_number, pattern_size_turple, square_size,
+                                     image_save_path)
+    
+    np.savez(camera_parameter_save_path, K=K, distCoeffs=distCoeffs)
 
 
-    if is_calibrate_camera == 1:
-        # 进行相机内参标定，路径为参数的保存路径
-        camera_parameter_save_path = os.path.join(parameter_save_path, camera_parameter)
-        K, distCoeffs = calibrate_camera(image_path, image_number, serial_number, pattern_size_turple, square_size,
-                                         image_save_path)
-
-        np.savez(camera_parameter_save_path, K=K, distCoeffs=distCoeffs)
-
-    else:
-        # 不进行相机内参标定，路径为读取相机内参路径
-        camera_parameter_path = os.path.join(parameter_save_path, camera_parameter)
-        K, distCoeffs = load_camera_parameters(camera_parameter_path)
+    # if is_calibrate_camera == 1:
+    #     # 进行相机内参标定，路径为参数的保存路径
+    #     camera_parameter_save_path = os.path.join(parameter_save_path, camera_parameter)
+    #     K, distCoeffs = calibrate_camera(image_path, image_number, serial_number, pattern_size_turple, square_size,
+    #                                      image_save_path)
+    #
+    #     np.savez(camera_parameter_save_path, K=K, distCoeffs=distCoeffs)
+    #
+    # else:
+    #     # 不进行相机内参标定，路径为读取相机内参路径
+    #     camera_parameter_path = os.path.join(parameter_save_path, camera_parameter)
+    #     K, distCoeffs = load_camera_parameters(camera_parameter_path)
 
     '''
     image_folder, image_number, K, distCoeffs,board_type='chessboard',
@@ -256,7 +262,7 @@ def calibrate_(user_input):
     t_mark2camera, R_mark2camera = compute_mark2camera(image_path, image_number, K, distCoeffs, serial_number=serial_number,
                                                        board_type='chessboard', pattern_size = pattern_size_turple,
                                                        square_size = square_size,
-                                                       save_corner = True, save_path=image_save_path)
+                                                       save_corner = False, save_path=None)
     t_gripper2base, R_gripper2base = compute_gripper2base(poses_list)
 
     T_camera2gripper = calilbrate_eye_in_hand(R_gripper2base, t_gripper2base,R_mark2camera, t_mark2camera,
