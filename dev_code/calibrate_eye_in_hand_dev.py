c7a3f36a5be142bb04e84d9cc7cf8cfd72f2fd7c
何广鹏 <86814805+naixupan@users.noreply.github.com>
2025-06-17 17:37:03 +0800

    手眼标定功能添加：相对位置计算

--- dev_code/calibrate_eye_in_hand_dev.py
+++ dev_code/calibrate_eye_in_hand_dev.py
@@ -208,68 +208,109 @@ def calibrate_(user_input):
     logging.info(f'输入参数：{config}')
 
     try:
-        image_path = config.get("image_path")   # 图像文件夹路径
-        image_number = config.get("image_number")   # 图像数量
-        pose_file_name = config.get("pose_file_name")   # 机械臂位姿文件名称
-        parameter_save_path = config.get("parameter_save_path")     # 参数保存路径
-        image_save_path = config.get("image_save_path")     #手眼标定结果图片保存路径
-        pattern_size = config.get("pattern_size")  # 标定板规格，类型为字符串如"9 16"，使用空格隔开
-        square_size = config.get("square_size")     # 标定板单位尺寸，类型为字符串
-        serial_number = config.get("serial_number") # 序列号
+
+        calibrate_type = config['calibrate_type']
+        # 1为手眼标定模式
+        if calibrate_type == '1':
+
+            image_path = config.get("image_path")   # 图像文件夹路径
+            image_number = config.get("image_number")   # 图像数量
+            pose_file_name = config.get("pose_file_name")   # 机械臂位姿文件名称
+            parameter_save_path = config.get("parameter_save_path")     # 参数保存路径
+            image_save_path = config.get("image_save_path")     #手眼标定结果图片保存路径
+            pattern_size = config.get("pattern_size")  # 标定板规格，类型为字符串如"9 16"，使用空格隔开
+            square_size = config.get("square_size")     # 标定板单位尺寸，类型为字符串
+            serial_number = config.get("serial_number") # 序列号
+
+        # 2为计算相对位置
+        elif calibrate_type == '2':
+            image_path = config.get("image_path")
+            target_pose = config.get("target_pose")
+            robot_pose = config.get("robot_pose")
+            camera_prameter_path = config.get("camera_prameter_path")
+            hand_eye_prameter_path = config.get("hand_eye_prameter_path")
+            # 计算类型分为第一阶段拍照点计算和第二阶段放料位置的计算
+            comput_type = config.get("comput_type")
+            parameter_save_path = config.get("parameter_save_path")
 
     except Exception as e:
         logging.error(f'[{e}')
         error_code = 402
         error_message = 'Your input is invalid ! '
         return result_data
+    if calibrate_type == '1':
+        pose_file_path = os.path.join(image_path, pose_file_name)
+        calibrate_patameter_save_path = f"{parameter_save_path}/camera2gripper_{serial_number}_chess.npy"
+        camera_parameter_save_path = f"{parameter_save_path}/camera_parameter_{serial_number}_chess.npz"
+        image_number = int(image_number)
+        pattern_size_list = pattern_size.split(' ')
+        pattern_size_turple = tuple(int(data) for data in pattern_size_list)
+        print(pattern_size_turple)
+        square_size = float(square_size)
+        poses_list = read_robot_poses(pose_file_path)
+        K, distCoeffs = calibrate_camera(image_path, image_number, serial_number, pattern_size_turple, square_size,
+                                         image_save_path)
+        np.savez(camera_parameter_save_path, K=K, distCoeffs=distCoeffs)
+        t_mark2camera, R_mark2camera = compute_mark2camera(image_path, image_number, K, distCoeffs, serial_number=serial_number,
+                                                           board_type='chessboard', pattern_size = pattern_size_turple,
+                                                           square_size = square_size,
+                                                           save_corner = False, save_path=None)
+        t_gripper2base, R_gripper2base = compute_gripper2base(poses_list)
+        T_camera2gripper = calilbrate_eye_in_hand(R_gripper2base, t_gripper2base,R_mark2camera, t_mark2camera,
+                                                  calibrate_patameter_save_path)
+        logging.info("手眼标定结果测试")
+        clibrate_test(R_gripper2base, t_gripper2base, R_mark2camera, t_mark2camera,T_camera2gripper,image_number)
+    elif calibrate_type == '2':
 
-    pose_file_path = os.path.join(image_path, pose_file_name)
-    calibrate_patameter_save_path = f"{parameter_save_path}/camera2gripper_{serial_number}_chess.npy"
-    camera_parameter_save_path = f"{parameter_save_path}/camera_parameter_{serial_number}_chess.npz"
-    image_number = int(image_number)
+        K, distCoeffs = load_camera_parameters(camera_prameter_path)
+        cam2gripperMatrix = load_calibrate_matrix(hand_eye_prameter_path)
 
+        aruco_image_path1 = f'{image_path}-1.bmp'
+        aruco_image_path2 = f'{image_path}-2.bmp'
+        aruco_image_path3 = f'{image_path}-3.bmp'
+        logging.info(f'image_path1:{aruco_image_path1}')
+        logging.info(f'image_path2:{aruco_image_path2}')
+        logging.info(f'image_path3:{aruco_image_path3}')
+        aruco_image_path = [aruco_image_path1, aruco_image_path2, aruco_image_path3]
+        robot_pose_list = robot_pose.split(' ')
+        target_pose_list = target_pose.split(' ')
+        robot_pose_f = [0,0,0,0,0,0]
+        target_pose_f = [0,0,0,0,0,0]
+        for i in range(6):
+            robot_pose_f[i] = float(robot_pose_list[i])
+            target_pose_f[i] = float(target_pose_list[i])
+        gripper2base = Pose2HomogeneousMatrix(robot_pose_f)
+        total_pose = [0, 0, 0, 0, 0, 0]
+        avg_pose = [0, 0, 0, 0, 0, 0]
+        for i in range(len(aruco_image_path)):
+            mark2camera = compute_aruco_pose(aruco_image_path[i], K, distCoeffs, 0.03, False, None)
+            mark2base = gripper2base @ cam2gripperMatrix @ mark2camera
+            mark_pose = HomogeneousMatrix2Pose(mark2base)
+            logging.info(f'mark_pose_{i}:{mark_pose}')
+            for j in range(6):
+                total_pose[j] += target_pose[j]
+        for i in range(6):
+            avg_pose[i] = total_pose[i] / 3
+        logging.info(f'avg_pose:{avg_pose}')
 
-    pattern_size_list = pattern_size.split(' ')
-    pattern_size_turple = tuple(int(data) for data in pattern_size_list)
-    
-    print(pattern_size_turple)
-    square_size = float(square_size)
-
-    poses_list = read_robot_poses(pose_file_path)
-    
-    K, distCoeffs = calibrate_camera(image_path, image_number, serial_number, pattern_size_turple, square_size,
-                                     image_save_path)
-    
-    np.savez(camera_parameter_save_path, K=K, distCoeffs=distCoeffs)
-
-
-    # if is_calibrate_camera == 1:
-    #     # 进行相机内参标定，路径为参数的保存路径
-    #     camera_parameter_save_path = os.path.join(parameter_save_path, camera_parameter)
-    #     K, distCoeffs = calibrate_camera(image_path, image_number, serial_number, pattern_size_turple, square_size,
-    #                                      image_save_path)
-    #
-    #     np.savez(camera_parameter_save_path, K=K, distCoeffs=distCoeffs)
-    #
-    # else:
-    #     # 不进行相机内参标定，路径为读取相机内参路径
-    #     camera_parameter_path = os.path.join(parameter_save_path, camera_parameter)
-    #     K, distCoeffs = load_camera_parameters(camera_parameter_path)
-
-    '''
-    image_folder, image_number, K, distCoeffs,board_type='chessboard',
-                        pattern_size = (9, 6), square_size = 0.02, save_corner = False, save_path=None'''
-    t_mark2camera, R_mark2camera = compute_mark2camera(image_path, image_number, K, distCoeffs, serial_number=serial_number,
-                                                       board_type='chessboard', pattern_size = pattern_size_turple,
-                                                       square_size = square_size,
-                                                       save_corner = False, save_path=None)
-    t_gripper2base, R_gripper2base = compute_gripper2base(poses_list)
-
-    T_camera2gripper = calilbrate_eye_in_hand(R_gripper2base, t_gripper2base,R_mark2camera, t_mark2camera,
-                                              calibrate_patameter_save_path)
-
-    logging.info("手眼标定结果测试")
-    clibrate_test(R_gripper2base, t_gripper2base, R_mark2camera, t_mark2camera,T_camera2gripper,image_number)
+        # 1表示计算水平拍照位到垂直拍照位的位姿
+        if comput_type == '1':
+            dx = target_pose_f[0] - avg_pose[0]
+            dy = target_pose_f[1] - avg_pose[1]
+            dz = target_pose_f[2] - avg_pose[2]
+            d_pose = [dx, dy, dz]
+            d_pose_arr = np.array(d_pose, dtype=float)
+            logging.info(f'拍照位1——>拍照位2 相对位姿:{d_pose_arr}')
+            parameter_save_name = f"{parameter_save_path}/shot_pose1_to_pose2.npy"
+            np.save(parameter_save_name, d_pose_arr)
+        # 2表示计算垂直拍照位到工作位的相对位姿
+        elif comput_type == '2':
+            T_mark2base = Pose2HomogeneousMatrix(avg_pose)
+            T_target2base = Pose2HomogeneousMatrix(target_pose)
+            T_target2mark = compute_target2mark_marix(T_mark2base, T_target2base)
+            logging.info(f'拍照位2——>工作位置 相对位姿:{T_target2mark}')
+            parameter_save_name = f"{parameter_save_path}/shot_pose2_to_target.npy"
+            np.save(parameter_save_name, T_target2mark)
 
 
 def main():
@@ -288,8 +329,6 @@ def main():
             logging.error(f"Error: {e}  0001")
             logging.info(f"error code: {error_code}")
             logging.info(f"error message: {error_message}")
-
-
 
 
 if __name__ == '__main__':
